34fca6865934978998e01ba5aded8e287a063264
Andreas <andreas@nowhere.local>
2014-01-20 21:59:11 +0100

    added controller class and generated accessors

--- src/de/andreaslehmann/securenotefx/presentation/noteeditor/NoteEditorPresenter.java
+++ src/de/andreaslehmann/securenotefx/presentation/noteeditor/NoteEditorPresenter.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.andreaslehmann.securenotefx.presentation.noteeditor;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,11 +19,16 @@ import org.slf4j.LoggerFactory;
  * @author Andreas
  */
 public class NoteEditorPresenter implements Initializable {
-        private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private HTMLEditor bodyEditor;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
     }
 
 }
