74efe4a07a6b8f51a569eb5849ecb600f4a8e3e1
Andreas <andreas@nowhere.local>
2014-01-20 21:57:02 +0100

    added note html editor view to project

---
+++ src/de/andreaslehmann/securenotefx/presentation/noteeditor/NoteEditorPresenter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.andreaslehmann.securenotefx.presentation.noteeditor;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Andreas
+ */
+public class NoteEditorPresenter implements Initializable {
+        private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+
+}
