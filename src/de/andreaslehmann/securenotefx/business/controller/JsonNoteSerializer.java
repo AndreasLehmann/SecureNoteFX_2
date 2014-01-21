a79f9ce1d4e1193016a6b1606d9fd6cc3f3d58cc
Andreas <andreas@nowhere.local>
2014-01-21 23:24:25 +0100

    build standanlone json serializer added tests

---
+++ src/de/andreaslehmann/securenotefx/business/controller/JsonNoteSerializer.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.andreaslehmann.securenotefx.business.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import de.andreaslehmann.securenotefx.business.entity.NoteEntity;
+import java.lang.reflect.Type;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author Andreas
+ */
+public class JsonNoteSerializer {
+
+    private final Gson gson;
+
+    JsonNoteSerializer() {
+        super();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(SimpleStringProperty.class, new SimpleStringPropertyTypeAdapter());
+        gsonBuilder.registerTypeAdapter(SimpleBooleanProperty.class, new SimpleBooleanPropertyTypeAdapter());
+        gson = gsonBuilder.create();
+    }
+
+    public String serialize(NoteEntity note) {
+        String json = gson.toJson(note);
+        return json;
+    }
+
+    public NoteEntity deserialize(String jsonString) {
+        NoteEntity n = (NoteEntity) gson.fromJson(jsonString, NoteEntity.class);
+        return n;
+    }
+    
+        private static class SimpleStringPropertyTypeAdapter implements JsonSerializer<SimpleStringProperty>, JsonDeserializer<SimpleStringProperty> {
+
+        @Override
+        public JsonElement serialize(SimpleStringProperty t, Type type, JsonSerializationContext jsc) {
+            return new JsonPrimitive(t.getValue());
+        }
+
+        @Override
+        public SimpleStringProperty deserialize(JsonElement je, Type type, JsonDeserializationContext jdc) throws JsonParseException {
+            return new SimpleStringProperty(je.getAsJsonPrimitive().getAsString());
+        }
+    }
+
+    private static class SimpleBooleanPropertyTypeAdapter implements JsonSerializer<SimpleBooleanProperty>, JsonDeserializer<SimpleBooleanProperty> {
+
+        @Override
+        public JsonElement serialize(SimpleBooleanProperty t, Type type, JsonSerializationContext jsc) {
+            return new JsonPrimitive(t.getValue());
+        }
+
+        @Override
+        public SimpleBooleanProperty deserialize(JsonElement je, Type type, JsonDeserializationContext jdc) throws JsonParseException {
+            return new SimpleBooleanProperty(je.getAsJsonPrimitive().getAsBoolean());
+        }
+    }
+
+}
---
+++ test/de/andreaslehmann/securenotefx/business/controller/JsonNoteSerializerTest.java
@@ -0,0 +1,58 @@
+package de.andreaslehmann.securenotefx.business.controller;
+
+import de.andreaslehmann.securenotefx.business.entity.NoteEntity;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andreas
+ */
+public class JsonNoteSerializerTest {
+
+    public JsonNoteSerializerTest() {
+    }
+
+    /**
+     * Test of serialize method, of class JsonNoteSerializer.
+     */
+    @Test
+    public void testSerialize() {
+        JsonNoteSerializer s = new JsonNoteSerializer();
+
+        NoteEntity n = new NoteEntity("A", "B");
+        String jsonString = s.serialize(n);
+        System.out.println(jsonString);
+        // {    "uniqueKey":"28f603e1-528b-44a0-b543-5043b93c469a",
+        //      "title":"A",
+        //      "body":"B",
+        //      "createdOn":1390342303292,
+        //      "lastSavedOn":0,
+        //      "deletedOn":0,
+        //      }
+        assertTrue(jsonString.contains("uniqueKey"));
+        assertTrue(jsonString.contains("title"));
+        assertTrue(jsonString.contains("body"));
+        assertTrue(jsonString.contains("deletedOn"));
+    }
+
+    /**
+     * Test of deserialize method, of class JsonNoteSerializer.
+     */
+    @Test
+    public void testDeserialize() {
+        final String JSON1 = "{\"uniqueKey\":\"26c1701e-0d3f-4c05-8cf7-1e460d41ebc9\",\"title\":\"A\",\"body\":\"B\",\"createdOn\":1390342698605,\"lastSavedOn\":0,\"deletedOn\":0}";
+
+        JsonNoteSerializer s = new JsonNoteSerializer();
+
+        NoteEntity n = s.deserialize(JSON1);
+
+        assertEquals("26c1701e-0d3f-4c05-8cf7-1e460d41ebc9", n.getUniqueKey().toString());
+        assertEquals("A", n.getTitle());
+        assertEquals("B", n.getBody());
+        assertFalse(n.isDirty());
+        assertFalse(n.isDeleted());
+
+    }
+
+}
